69a014f96aa00b7b4200613c979b8d22daa1f2c8
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2022-08-07 19:56:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,34 +4,38 @@ const LIST_URL = 'https://mate-academy.github.io/phone-catalogue-static/api/phon
 const DETAILS_URL = 'https://mate-academy.github.io/phone-catalogue-static/api/phones/';
 
 const ul = document.createElement('ul');
-const combinedData = [];
+const phonesWithDetails = [];
 
 const getData = (url) => {
   return fetch(url)
     .then(res => {
       if (!res.ok) {
-        return Promise.reject(`${res.status} and some other error text`)
+        return setTimeout(() => {
+          Promise.reject(`${res.status} and some other error text`);
+        }, 5000);
+        
       };
-
+  
       if (!res.headers.get('content-type').includes('application/json')) {
-        return Promise.reject('Content type is not supported');
-      }
+        return setTimeout(() => {
+          Promise.reject('Content type is not supported');
+        }, 5000);
+      };
 
       return res.json();
     });
 };
 
 const getPhones = () => {
-  setTimeout((
-    getData(LIST_URL)
-      .then(result => {
-        return result.map(el => {
-          combinedData.push(el);
+  getData(LIST_URL)
+    .then(result => {
+      return result.map(el => {
+        phonesWithDetails.push(el);
 
-          return getPhonesDetails(el.id);
-        });
-      })
-  ), 5000);
+        return getPhonesDetails(el.id);
+      });
+    })
+
 };
 
 const getPhonesDetails = (id) => {
@@ -39,7 +43,7 @@ const getPhonesDetails = (id) => {
     .then(result => {
       const li = document.createElement('li');
 
-      combinedData.push(result);
+      phonesWithDetails.push(result);
 
       li.innerText = result.name;
       ul.append(li);
@@ -48,4 +52,3 @@ const getPhonesDetails = (id) => {
 
 document.body.append(ul);
 getPhones();
-console.log({combinedData});
